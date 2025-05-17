e32928d268e665208bf6e439dbf1785966c37ff5
IT24510004 <it24510004@my.sliit.lk>
2025-05-17 21:00:05 +0530

    Add User base class with common fields, constructors, and getters/setters

--- src/main/java/com/yourteam/appointment/model/User.java
+++ src/main/java/com/yourteam/appointment/model/User.java
@@ -1,85 +1,35 @@
 package com.yourteam.appointment.model;
 
 public class User {
-    private String Name;
-    private String Password;
-    private String Email;
-    private String PhoneNumber;
-    private String Nic;
-    private String gender;
-    private int age;
+    protected String name;
+    protected String email;
+    protected String password;
+    protected String nic;
+    protected String gender;
 
-    public User() {
-    }
+    public User() {}
 
     public User(String name, String email, String password, String nic, String gender) {
-
-    }
-
-    public User(String Name, int age, String gender, String Nic, String phoneNumber, String email, String password) {
-        Name = Name;
-        this.age = age;
-        this.gender = gender;
-        this.Nic = Nic;
-        PhoneNumber = phoneNumber;
-        Email = email;
-        Password = password;
-    }
-
-    public String getName() {
-        return Name;
-    }
-
-    public void setName(String name) {
-        Name = name;
-    }
-
-    public String getPassword() {
-        return Password;
-    }
-
-    public void setPassword(String password) {
-        Password = password;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
-    public String getPhoneNumber() {
-        return PhoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        PhoneNumber = phoneNumber;
-    }
-
-    public String getNic() {
-        return Nic;
-    }
-
-    public void setNic(String Nic) {
-        this.Nic = this.Nic;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.nic = nic;
         this.gender = gender;
     }
 
-    public int getAge() {
-        return age;
-    }
+    // Getters and setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getNic() { return nic; }
+    public void setNic(String nic) { this.nic = nic; }
+
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
 }
